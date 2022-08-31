9c3981999b0b227b4ea3a2086f9746f260355c89
Isis Zapata <isis.zapata.hn@gmail.com>
2022-08-31 16:32:20 -0600

    .
    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+const messages = [
+    "Hi! I'm Isis Zapata and I'm a software engineer.",
+    "I'm full-stack developer.",
+    "I'm From Honduras",
+    "I love to code.",
+    "I'm a beautiful person.",
+    "Actually, I'm a programmer.",
+    "I'm Happy to be a programmer.",
+    "Weight 43kg",
+    "Computer Science Engineer in University Catholic of Honduras",
+    "My name is Isis Zapata and I'm a software engineer.",
+    "Github: isinicolle",
+    "LinkedIn: isis-zapata",
+  ];
+  
+  const funnyCommit = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(`\x1b[34m${message}\x1b[89m`);
+  }
+  
+  module.exports = {
+    funnyCommit
+  };
